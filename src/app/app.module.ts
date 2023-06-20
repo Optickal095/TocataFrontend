03b25c3d21545acd393b052b0e45676033c3c6f9
Eduardo Hernández Oyarzún <eduardo.he095@gmail.com>
2023-06-19 21:41:20 -0400

    homeComponentTest

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent, RegisterComponent],
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-home',
+  selector: 'home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  public title: string;
 
+  constructor() {
+    this.title = 'Bienvenido a Tocata';
+  }
+
+  ngOnInit(): void {
+    console.log('home.component cargado!!');
+  }
 }
